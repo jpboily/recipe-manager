28e24d1217e5292e0d14976bd00fa34322238120
jpboily@gmail.com <u93o4a86>
2016-03-04 11:01:15 -0500

    Updated About, Contact and Help page.

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Recipe Manager"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Recipe Manager"
+  end
+
+  test "should get help" do
+      get :help
+      assert_response :success
+      assert_select "title", "Help | Recipe Manager"
+  end
+
+end
