2264e097aea851ae6cb2afbcc851a1a50a81ff79
jpboily@gmail.com <u93o4a86>
2016-03-08 15:12:58 -0500

    Added secure password to users using has_secure_password method. Password length of 6 chars minimum and not blank is forced.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304210755) do
+ActiveRecord::Schema.define(version: 20160308195322) do
 
   create_table "ingredients", force: :cascade do |t|
     t.float    "qty"
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 20160304210755) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
